f95115e4435f14eaf93dbd36aac84ca76e003e5e
Maksim Bezsaznyj <bezmax@gmail.com>
2022-01-17 12:24:39 -0500

    Add .circleci/config.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,27 @@
+repositories {
+  mavenCentral()
+}
+
 buildscript {
+  repositories {
+    mavenCentral()
+  }
+
   dependencies {
     with(chronicler.Dependencies) {
       classpath(kotlinGradlePlugin)
       classpath(kotlinxCoroutines)
       classpath(junitGradlePlugin)
-      classpath(shadowJarPlugin)
       classpath(wireGradlePlugin)
     }
   }
 }
 
 subprojects {
+  repositories {
+    mavenCentral()
+  }
+
   apply(plugin = "java")
   apply(plugin = "kotlin")
 }
